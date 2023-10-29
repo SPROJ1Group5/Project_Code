94ed83bc1333e0d24ff61b1758c2c8115c8cc20c
Bence Tóth <bencike04201@gmail.com>
2023-10-29 02:05:39 +0100

    Uploaded a bit improved version.
    I changed the polling of the button to an Interrupt handler, now only reacting to a FALLING edge signal, with an additional bounce-off delay (without that it is unreliable).

--- SPROJ1G5_Multimeter_Code.ino
+++ SPROJ1G5_Multimeter_Code.ino
@@ -1,89 +1,69 @@
+#include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 //#include <EEPROM.h>
 
 //pin numbering may change throughout the project
-const byte total_modes = 5 , continuityPin = 5 , freqPin = 8 , buzzerPin = 4 , buttonPin = 13 , LCD_I2C_ADDR = 0x27 , LCD_ROWS = 20 , LCD_COLS = 4 , bounce_off_delay = 130 ;
-byte mode_select = 0 , previous_mode = 0 ;
+const byte total_modes = 5 , continuityPin = 5 , ledPin = 13 , freqPin = 8 , buzzerPin = 4 ,
+           buttonPin = 3 , LCD_I2C_ADDR = 0x27 , LCD_ROWS = 20 , LCD_COLS = 4 , bounceDelay = 150 ;
+byte mode_select = 0 ;
+volatile bool changed = false ;
 const unsigned int buzzerTone = 2000 ;
-bool LCD_cleared = false ;
-unsigned long highPeriod , lowPeriod ; //for the frequency measurement
-float frequency_value , voltage_conversion_factor = 20 / 1023 , amperage_conversion_factor = 500 / 1023 ; //in [Hz], [V] and [mA]
-String to_show ;
+unsigned long timeout = 500000 , highPeriod , lowPeriod ; //for the frequency measurement
+float frequency , voltage_conversion_factor = 20 / 1023 , amperage_conversion_factor = 500 / 1023 ; //in [Hz], [V] and [mA]
 
 LiquidCrystal_I2C LCD ( LCD_I2C_ADDR , LCD_ROWS , LCD_COLS ) ; //SDA - Pin A4 | SCL - Pin A5
 
-enum options {
-  NOT_CHOSEN = 0 ,
-  VOLT = 1 ,
-  AMP = 2 ,
-  OHM = 3 ,
-  FREQ = 4 ,
-  CONT = 5 } ;
+void buttonPressed ( void ) {
+  changed = true ; }
 
-void checkButtonState ( void ) {
-    //this function polls the button and increments the mode_select variable based on the state
-    if ( !digitalRead ( buttonPin ) ) {
-      mode_select++ ;
-      if ( mode_select > total_modes ) {
-        mode_select = 1 ; }
-      delay ( bounce_off_delay ) ; } }
+void frequencyTest ( void ) {
+  highPeriod = pulseIn ( freqPin , HIGH , timeout ) ;
+  lowPeriod = pulseIn ( freqPin , LOW , timeout ) ;
+  frequency = 1000000.0 / ( highPeriod + lowPeriod ) ;         //in Hz
+  LCD.setCursor ( 0 , 2 ) ; 
+  LCD.print ( String ( ( int ) frequency ) + " Hz                " ) ; }
 
 void continuityTest ( void ) {
-    //there is some minor bug regarding switching mode when buzzer is on, checking the state with a boolean variable solves it, but interferes with the sound of the buzzer
-    if ( !digitalRead ( continuityPin ) ) tone ( buzzerPin , buzzerTone ) ; 
-    else noTone ( buzzerPin ) ; }
+
+    if ( digitalRead ( continuityPin ) ) {
+      digitalWrite ( ledPin , HIGH ) ;
+      tone ( buzzerPin , buzzerTone ) ; 
+      LCD.setCursor( 0 , 2 ) ;
+      LCD.print ( "     CONNECTED      " ) ; }
+
+    else {
+      digitalWrite ( ledPin , LOW ) ; 
+      noTone ( buzzerPin ) ;
+      LCD.setCursor( 0 , 2 ) ;
+      LCD.print ( "                    " ) ; } }
 
 void setup ( ) {
-    pinMode ( continuityPin , INPUT_PULLUP ) ;
-    //pinMode ( freqPin , INPUT ) ;
-    pinMode ( buttonPin , INPUT_PULLUP ) ; //built-in pull-up resistor
+    pinMode ( buttonPin , INPUT_PULLUP ) ;
+    attachInterrupt ( digitalPinToInterrupt ( buttonPin ) , buttonPressed , FALLING ) ;
+    pinMode ( continuityPin , INPUT ) ;
+    pinMode ( freqPin , INPUT ) ;
     pinMode ( buzzerPin , OUTPUT ) ;
     LCD.init ( ) ;
     LCD.backlight ( ) ;
-    LCD.clear ( ) ;
-    LCD.setCursor ( 0 , 0 ) ;    
-    LCD.print ( "SPROJ1G5  MULTIMETER" ) ;
-    LCD.setCursor ( 0 , 1 ) ;    
-    LCD.print ( "      MAIN MENU      " ) ;
-    LCD.setCursor ( 0 , 2 ) ;    
-    LCD.print ( "Cycle through modes " ) ;
-    LCD.setCursor ( 0 , 3 ) ;    
-    LCD.print ( "  with the button.  " ) ; }
+    LCD.clear ( ) ; }
 
 void loop ( ) {
-    checkButtonState ( ) ;
 
-    if ( mode_select != NOT_CHOSEN && mode_select != previous_mode ) {
-      previous_mode = mode_select ;
-      if ( !LCD_cleared ) {
-        LCD_cleared = true ;
-        LCD.clear ( ) ; }
-
-      switch ( mode_select ) {
-        case VOLT : to_show = "      Voltmeter     " ; break ;
-        case AMP : to_show = "     Ampermeter     " ; break ;
-        case OHM : to_show = "      Ohm-meter     " ; break ;
-        case FREQ : to_show = "   Frequency-meter  " ; break ;
-        case CONT : to_show = "  Continuity test   " ; break ;
-        default : break ;
-      }
-
-      LCD.setCursor ( 0 , 0 ) ;
-      LCD.print ( to_show ) ; 
-    }
+    if ( changed ) {
+      delay ( bounceDelay ) ;
+      changed = false ;
+      digitalWrite ( ledPin , LOW ) ;
+      LCD.clear ( ) ;
+      noTone ( buzzerPin ) ;
+      mode_select++ ;
+      if ( mode_select == total_modes ) mode_select = 0 ; }
 
     switch ( mode_select ) {
-        case CONT : continuityTest ( ) ; break ;
-        default : break ;
+      case 0 : LCD.setCursor ( 0 , 0 ) ; LCD.print ( "      Voltmeter     " ) ; break ;
+      case 1 : LCD.setCursor ( 0 , 0 ) ; LCD.print ( "     Ampermeter     " ) ; break ;
+      case 2 : LCD.setCursor ( 0 , 0 ) ; LCD.print ( "      Ohm-meter     " ) ; break ;
+      case 3 : LCD.setCursor ( 0 , 0 ) ; LCD.print ( "   Frequency-meter  " ) ; frequencyTest ( ) ;  break ;
+      case 4 : LCD.setCursor ( 0 , 0 ) ; LCD.print ( "  Continuity test   " ) ; continuityTest ( ) ; break ;
+      default : break ; }
 
-
-    }
-
-
-    //Have to try out this one
-    /*highPeriod = pulseIn ( freqPin , HIGH ) ; 
-    lowPeriod = pulseIn ( freqPin , LOW ) ;
-    frequency = 1 / ( ( highTime + lowTime ) * 1000000 ) ; //given in Microseconds, so it needs to be converted to seconds*/
-    }
-
-
+  }
