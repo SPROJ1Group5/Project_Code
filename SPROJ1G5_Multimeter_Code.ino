e1ed18a23a57eae232fc221686e029aef94a57e7
Bence Tóth <bencike04201@gmail.com>
2023-10-30 02:17:36 +0100

    Add files via upload
    Included the temperature section front-end.  Played around with the length of the delay and the timeout in the pulseIn function to speed up measurement change.

--- SPROJ1G5_Multimeter_Code.ino
+++ SPROJ1G5_Multimeter_Code.ino
@@ -3,12 +3,12 @@
 //#include <EEPROM.h>
 
 //pin numbering may change throughout the project
-const byte total_modes = 5 , continuityPin = 5 , ledPin = 13 , freqPin = 8 , buzzerPin = 4 ,
-           buttonPin = 3 , LCD_I2C_ADDR = 0x27 , LCD_ROWS = 20 , LCD_COLS = 4 , bounceDelay = 150 ;
+const byte total_modes = 6 , continuityPin = 5 , freqPin = 8 , buzzerPin = 4 ,
+           buttonPin = 3 , LCD_I2C_ADDR = 0x27 , LCD_ROWS = 20 , LCD_COLS = 4 , bounceDelay = 100 ;
 byte mode_select = 0 ;
 volatile bool changed = false ;
 const unsigned int buzzerTone = 2000 ;
-unsigned long timeout = 500000 , highPeriod , lowPeriod ; //for the frequency measurement
+unsigned long timeout = 100000 , highPeriod , lowPeriod ; //for the frequency measurement
 float frequency , voltage_conversion_factor = 20 / 1023 , amperage_conversion_factor = 500 / 1023 ; //in [Hz], [V] and [mA]
 
 LiquidCrystal_I2C LCD ( LCD_I2C_ADDR , LCD_ROWS , LCD_COLS ) ; //SDA - Pin A4 | SCL - Pin A5
@@ -26,13 +26,11 @@ void frequencyTest ( void ) {
 void continuityTest ( void ) {
 
     if ( digitalRead ( continuityPin ) ) {
-      digitalWrite ( ledPin , HIGH ) ;
       tone ( buzzerPin , buzzerTone ) ; 
       LCD.setCursor( 0 , 2 ) ;
       LCD.print ( "     CONNECTED      " ) ; }
 
     else {
-      digitalWrite ( ledPin , LOW ) ; 
       noTone ( buzzerPin ) ;
       LCD.setCursor( 0 , 2 ) ;
       LCD.print ( "                    " ) ; } }
@@ -52,7 +50,6 @@ void loop ( ) {
     if ( changed ) {
       delay ( bounceDelay ) ;
       changed = false ;
-      digitalWrite ( ledPin , LOW ) ;
       LCD.clear ( ) ;
       noTone ( buzzerPin ) ;
       mode_select++ ;
@@ -64,6 +61,7 @@ void loop ( ) {
       case 2 : LCD.setCursor ( 0 , 0 ) ; LCD.print ( "      Ohm-meter     " ) ; break ;
       case 3 : LCD.setCursor ( 0 , 0 ) ; LCD.print ( "   Frequency-meter  " ) ; frequencyTest ( ) ;  break ;
       case 4 : LCD.setCursor ( 0 , 0 ) ; LCD.print ( "  Continuity test   " ) ; continuityTest ( ) ; break ;
+      case 5 : LCD.setCursor ( 0 , 0 ) ; LCD.print ( "  Temperature test  " ) ; break ;
       default : break ; }
 
   }
