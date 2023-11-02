04f47044bbd9a03700c9253bd6f1b94ee1872fc5
Bence Tóth <bencike04201@gmail.com>
2023-11-02 18:42:39 +0100

    Made some changes to the variables.

--- SPROJ1G5_Multimeter_Code.ino
+++ SPROJ1G5_Multimeter_Code.ino
@@ -5,44 +5,45 @@
 
 //pin numbering may change throughout the project
 const byte total_modes = 6 , continuityPin = 5 , freqPin = 12 , buzzerPin = 4 , INA_ADDR = 0x40 ,
-           buttonPin = 3 , LCD_I2C_ADDR = 0x27 , LCD_ROWS = 20 , LCD_COLS = 4 , bounceDelay = 150 ;
-byte mode_select = 0 , printing_poll = 0 , idx ;
-const byte samples = 5 ;
+           buttonPin = 3 , LCD_I2C_ADDR = 0x27 , LCD_ROWS = 20 , LCD_COLS = 4 , bounceDelay = 170 ;
+byte mode_select = 0 , printing_poll = 0 , samples , idx ;
 volatile bool changed = false ;
 const unsigned int buzzerTone = 2000 ;
-unsigned long highPeriod , lowPeriod ; //for the frequency measurement
-float upper_correction_limit = 30000.0 , freq_switch_limit = 100.0 , frequency , readout , voltage_conversion_factor = 20 / 1023 ,
-      calib_factor = 1.05 , milliseconds = 1000000.0 , current , amperage_conversion_factor = 500 / 1023 ; //in [V] and [mA]
+unsigned long highPeriod , lowPeriod ;
+float upper_correction_limit = 30000.0 , freq_switch_limit = 100.0 , frequency , readout , volt_conv_fact = 20 / 1023 ,
+      freq_calib_factor = 1.0555 , current_calib_factor = 0.86 , millisecs = 1000000.0 , current , amp_conv_fact = 500 / 1023 ; //in [V] and [mA]
 
-LiquidCrystal_I2C LCD ( LCD_I2C_ADDR , LCD_ROWS , LCD_COLS ) ; //SDA - Pin A4 | SCL - Pin A5
-Adafruit_INA219 INA219 ( INA_ADDR ) ;
+LiquidCrystal_I2C LCD ( LCD_I2C_ADDR , LCD_ROWS , LCD_COLS ) ;      //SDA - Pin A4
+Adafruit_INA219 INA219 ( INA_ADDR ) ;                               //SCL - Pin A5
 
 void buttonPressed ( void ) {
   changed = true ; }
 
-void currentTest ( void ) {
-  current = 0 ;
+void currentTest ( void ) {     //min 3 mA
+  current = 0 ;                 //max. 2A
+  samples = 255 ;
   for ( idx = 0 ; idx < samples ; idx++  ) {
     current += INA219.getCurrent_mA ( ) ; }
-  readout = current / ( float ) samples ;
+  readout = ( current / ( float ) samples ) * current_calib_factor ;
   LCD.setCursor ( 0 , 2 ) ;
   LCD.print ( String ( readout ) + " mA         " ) ; }
 
 void frequencyTest ( void ) {     //3Hz -> Lower-Bound Frequency
   printing_poll += 1 ;            //~100 kHz -> Upper-Bound Frequency (+/- 5% error)
   frequency = 0 ;
+  samples = 5 ;
   for ( idx = 0 ; idx < samples ; idx++  ) {
     highPeriod = pulseIn ( freqPin , HIGH ) ;
     lowPeriod = pulseIn ( freqPin , LOW ) ;
-    frequency += milliseconds / ( ( ( float ) highPeriod + ( float ) lowPeriod ) ) ; }
+    frequency += millisecs / ( ( ( float ) highPeriod + ( float ) lowPeriod ) ) ; }
   readout = frequency / ( float ) samples ;
-  if ( readout >= upper_correction_limit ) readout *= calib_factor ;
+  if ( readout >= upper_correction_limit ) readout *= freq_calib_factor ;
   if ( ( printing_poll == 20 && readout > freq_switch_limit ) || readout <= freq_switch_limit ) {
     printing_poll = 0 ;
     LCD.setCursor ( 0 , 2 ) ; 
     LCD.print ( String ( readout ) + " Hz              " ) ; } }
 
-void continuityTest ( void ) {
+void continuityTest ( void ) {      //14.2kΩ cutoff resistance
 
     if ( digitalRead ( continuityPin ) ) {
       tone ( buzzerPin , buzzerTone ) ; 
@@ -82,6 +83,4 @@ void loop ( ) {
       case 3 : LCD.setCursor ( 0 , 0 ) ; LCD.print ( "   Frequency-meter  " ) ; frequencyTest ( ) ;  break ;
       case 4 : LCD.setCursor ( 0 , 0 ) ; LCD.print ( "  Continuity test   " ) ; continuityTest ( ) ; break ;
       case 5 : LCD.setCursor ( 0 , 0 ) ; LCD.print ( "  Temperature test  " ) ; break ;
-      default : break ; }
-
-  }
+      default : break ; } }
