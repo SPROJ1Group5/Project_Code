19d2f397ab3469a27fea15259e7c20ed7604df4a
Bence Tóth <bencike04201@gmail.com>
2023-11-09 05:24:42 +0100

    Add files via upload
    Wrote an experimental sketch for the resistance measurement with autoranging.

--- SPROJ1G5_Multimeter_Code.ino
+++ SPROJ1G5_Multimeter_Code.ino
@@ -4,17 +4,55 @@
 //#include <EEPROM.h>
 
 //pin numbering may change throughout the project
-const byte total_modes = 6 , continuityPin = 5 , freqPin = 6 , buzzerPin = 4 , INA_ADDR = 0x40 ,
-           nextButtonPin = 3 , prevButtonPin = 2 , LCD_ADDR = 0x27 , LCD_ROWS = 20 , LCD_COLS = 4 , bounceDelay = 170 ;
-byte mode_select = 1 , printing_poll = 0 , samples , idx ;
+const byte total_modes = 6 , continuityPin = 5 , freqPin = 6 , buzzerPin = 4 , INA_ADDR = 0x40 , resistanceAdcPin = A0 ,
+           nextButtonPin = 3 , prevButtonPin = 2 , LCD_ADDR = 0x27 , LCD_ROWS = 20 , LCD_COLS = 4 , bounceDelay = 160 , resistanceRanges = 5 ;
+const byte resistanceSelectPins [ 5 ] = { 12 , 11 , 10 , 9 , 8 } ;
+byte mode_select = 1 , printing_poll = 0 , idx , samples , resHalfIndex ;
 volatile bool next = false , prev = false ;
 const unsigned int buzzerTone = 2000 ;
-unsigned long highPeriod , lowPeriod ;
-float upper_correction_limit = 30000.0 , freq_switch_limit = 100.0 , frequency , readout , volt_conv_fact = 20 / 1023 ,
-      freq_calib_factor = 1.0555 , current_calib_factor = 0.86 , millisecs = 1000000.0 , current , amp_conv_fact = 500 / 1023 ; //in [V] and [mA]
+int resistanceMeasuredValues [ 5 ] ;
+float resistanceFactors [ 5 ] = { 100 / 1023 , 1000 / 1023 , 10000 / 1023 , 100000 / 1023 , 1000000 / 1023 } ;
+unsigned long highPeriod , lowPeriod , totalPeriod ;
+float upper_correction_limit = 30000.0 , freq_switch_limit = 100.0 , frequency , readout , resMultiplyFactor , contCutoff = 10.0 ,
+      freq_calib_factor = 1.0555 , current_calib_factor = 0.86 , millisecs = 1000000.0 , current , resDivider = 1.0 ;
+String resUnit ;
 
 LiquidCrystal_I2C LCD ( LCD_ADDR , LCD_ROWS , LCD_COLS ) ;      //SDA - Pin A4
-Adafruit_INA219 INA219 ( INA_ADDR ) ;                               //SCL - Pin A5
+Adafruit_INA219 INA219 ( INA_ADDR ) ;                           //SCL - Pin A5
+
+void setResistancePin ( byte onPin ) {
+  for ( byte g = 0 ; g < 5 ; g++ ) {
+    if ( g == onPin ) digitalWrite ( resistanceSelectPins [ g ] , LOW ) ;
+    else digitalWrite ( resistanceSelectPins [ g ] , HIGH ) ; } }
+
+byte getClosestToHalf ( int * array , const byte size ) {
+  int minValue = abs ( array [ 0 ] - 512 ) ;
+  byte minIndex = 0 ;
+  for ( byte i = 1 ; i < size ; i++ ) {
+    if ( abs ( array [ i ] - 512 ) < minValue ) {
+      minValue = array [ i ] ;
+      minIndex = i ; } }
+  return minIndex; }
+
+void resistanceTest ( void ) {
+  samples = 5 ;
+  for ( idx = 0 ; idx < samples ; idx++ ) {
+    setResistancePin ( idx ) ;
+    resistanceMeasuredValues [ idx ] = analogRead ( resistanceAdcPin ) ; }
+
+  resHalfIndex = getClosestToHalf ( resistanceMeasuredValues , resistanceRanges ) ;
+
+  switch ( resHalfIndex ) {
+    case 0  : resMultiplyFactor = resistanceFactors [ 0 ] ; resUnit = "Ω"  ; resDivider = 1.0       ; break ;
+    case 1  : resMultiplyFactor = resistanceFactors [ 1 ] ; resUnit = "Ω"  ; resDivider = 1.0       ; break ;
+    case 2  : resMultiplyFactor = resistanceFactors [ 2 ] ; resUnit = "kΩ" ; resDivider = 1000.0    ; break ;
+    case 3  : resMultiplyFactor = resistanceFactors [ 3 ] ; resUnit = "kΩ" ; resDivider = 1000.0    ; break ;
+    case 4  : resMultiplyFactor = resistanceFactors [ 4 ] ; resUnit = "MΩ" ; resDivider = 1000000.0 ; break ;
+    default : break ; }
+
+  readout = ( ( float ) resistanceMeasuredValues [ resHalfIndex ] * resMultiplyFactor ) / resDivider ;
+  LCD.setCursor ( 0 , 2 ) ;
+  LCD.print ( String ( readout ) + resUnit + "        " ) ; }
 
 void nextButtonPressed ( void ) {
   next = true ; }
@@ -27,33 +65,36 @@ void currentTest ( void ) {     //min 3 mA
   samples = 255 ;
   for ( idx = 0 ; idx < samples ; idx++  ) {
     current += INA219.getCurrent_mA ( ) ; }
-  readout = ( current / ( float ) samples ) * current_calib_factor ;
+  readout = ( current / (float) samples ) * current_calib_factor ;
   LCD.setCursor ( 0 , 2 ) ;
   LCD.print ( String ( readout ) + " mA         " ) ; }
 
 void frequencyTest ( void ) {     //3Hz -> Lower-Bound Frequency
   printing_poll += 1 ;            //~100 kHz -> Upper-Bound Frequency (+/- 5% error)
   frequency = 0 ;
-  samples = 5 ;
+  samples = 8 ;
   for ( idx = 0 ; idx < samples ; idx++  ) {
     highPeriod = pulseIn ( freqPin , HIGH ) ;
     lowPeriod = pulseIn ( freqPin , LOW ) ;
-    frequency += millisecs / ( ( ( float ) highPeriod + ( float ) lowPeriod ) ) ; }
-  readout = frequency / ( float ) samples ;
+    totalPeriod = highPeriod + lowPeriod ;
+    frequency += millisecs / (float) totalPeriod ; }
+  readout = frequency / (float) samples ;
   if ( readout >= upper_correction_limit ) readout *= freq_calib_factor ;
   if ( ( printing_poll == 20 && readout > freq_switch_limit ) || readout <= freq_switch_limit ) {
     printing_poll = 0 ;
-    LCD.setCursor ( 0 , 2 ) ; 
+    LCD.setCursor ( 0 , 2 ) ;
     LCD.print ( String ( readout ) + " Hz              " ) ; } }
 
-void continuityTest ( void ) {      //14.2kΩ cutoff resistance
+void continuityTest ( void ) {
 
-    if ( digitalRead ( continuityPin ) ) {
-      tone ( buzzerPin , buzzerTone ) ; 
-      LCD.setCursor( 0 , 2 ) ;
-      LCD.print ( "     CONNECTED      " ) ; }
+  setResistancePin ( 0 ) ;
 
-    else {
+  if ( ( (float) analogRead ( resistanceAdcPin ) * resistanceFactors [ 0 ] ) <= contCutoff ) {
+    tone ( buzzerPin , buzzerTone ) ; 
+    LCD.setCursor( 0 , 2 ) ;
+    LCD.print ( "     CONNECTED      " ) ; }
+  
+  else {
       noTone ( buzzerPin ) ;
       LCD.setCursor( 0 , 2 ) ;
       LCD.print ( "   NOT  CONNECTED   " ) ; } }
@@ -87,7 +128,7 @@ void loop ( ) {
     switch ( mode_select ) {
       case 1 : LCD.setCursor ( 0 , 0 ) ; LCD.print ( "      Voltmeter     " ) ; break ;
       case 2 : LCD.setCursor ( 0 , 0 ) ; LCD.print ( "     Ampermeter     " ) ; currentTest ( ) ; break ;
-      case 3 : LCD.setCursor ( 0 , 0 ) ; LCD.print ( "      Ohm-meter     " ) ; break ;
+      case 3 : LCD.setCursor ( 0 , 0 ) ; LCD.print ( "      Ohm-meter     " ) ; resistanceTest ( ) ; break ;
       case 4 : LCD.setCursor ( 0 , 0 ) ; LCD.print ( "   Frequency-meter  " ) ; frequencyTest ( ) ;  break ;
       case 5 : LCD.setCursor ( 0 , 0 ) ; LCD.print ( "  Continuity test   " ) ; continuityTest ( ) ; break ;
       case 6 : LCD.setCursor ( 0 , 0 ) ; LCD.print ( "  Temperature test  " ) ; break ;
